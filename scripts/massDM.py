d9166ad8b87712c3fca5eb104181fa38c930a0c0
Oogle Glu <avirukbasak@gmail.com>
2021-05-27 12:51:28 +0530

    update massDM.py

--- scripts/massDM.py
+++ scripts/massDM.py
@@ -55,16 +55,16 @@ def login (username, your_password):
 def send_message(msg):
    
     # Find message button
-    message = chrome.find_element_by_class_name('_862NM ') 
-    message.click()
-    time.sleep(2)
-    chrome.find_element_by_class_name('HoLwm ').click()
-    time.sleep(1)
-    for x in range(10):
-        mbox = chrome.find_element_by_tag_name('textarea')
-        mbox.send_keys(msg)
-        mbox.send_keys(Keys.RETURN)
-        time.sleep(1.2)
+    message = chrome.find_element_by_class_name ('_862NM ') 
+    message.click ()
+    time.sleep (2)
+    chrome.find_element_by_class_name ('HoLwm ').click()
+    time.sleep (1)
+    
+    mbox = chrome.find_element_by_tag_name('textarea')
+    mbox.send_keys(msg)
+    mbox.send_keys(Keys.RETURN)
+    time.sleep(1.2)
         
 # Driver code
 textfile = NULL
