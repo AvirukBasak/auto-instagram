de9fcdb3924acee89b13ab085647423c8e5fd226
Oogle Glu <avirukbasak@gmail.com>
2021-05-27 12:50:42 +0530

    update massDM.py

--- scripts/massDM.py
+++ scripts/massDM.py
@@ -1,3 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import selenium.common.exceptions
+import time
+
+# Login Credentials
+username = open ("credentials.txt", "r").readline ();
+password = open ("credentials.txt", "r").readline ();
+
+url = ""
+def path ():
+        global chrome
+        # starts a new headless chrome session
+        option = Options ()
+        # option.add_argument ("--headless")
+        option.add_argument ("window-size=1024x700")
+        try:
+            # Add path to chrome driver executable file if required
+            chrome = webdriver.Chrome ("data/chromedriver.exe", options=option)
+        except:
+            print ("Could not locate Chrome Driver")
+            exit ();
+        print ("Chrome ready")
+
+
+def url_name (url):
+    chrome.get (url)
+    # adjust sleep if you want
+    time.sleep (3)
+
+def login (username, your_password):
+    log_but = chrome.find_element_by_class_name ("L3NKy")
+    time.sleep (1)
+    log_but.click ()
+    time.sleep (1)
+     
+    # finds the username box
+    usern = chrome.find_element_by_name ("username")
+     
+    # sends the entered username
+    usern.send_keys (username)
+ 
+    # finds the password box
+    passw = chrome.find_element_by_name ("password")
+ 
+    # sends the entered password
+    passw.send_keys (your_password)
+     
+    # press enter after sending password
+    passw.send_keys (Keys.RETURN)
+    time.sleep (4)
+    print ("Logged in")
+
 def send_message(msg):
    
     # Find message button
